4298c12d652076da1ff57f43001ae885e3069d3f
Vladut Ilie <vladilie94@gmail.com>
2022-08-18 15:18:17 +0300

    Navbar component added.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export const Navbar: React.FC = () => {
+  return (
+    <div className='sticky top-0 bg-zinc-100'>
+      <nav className='container mx-auto flex max-w-3xl items-center justify-between px-4 py-2'>
+        <Link href=''>
+          <a className='inline'>
+            <h3>{process.env.NEXT_PUBLIC_SITE_NAME?.split(' ')[0]}</h3>
+          </a>
+        </Link>
+
+        <p>💻</p>
+      </nav>
+    </div>
+  );
+};
