b6aba3451c8e843defb28545f8b5d9816e182b23
Vladut Ilie <vladilie94@gmail.com>
2022-08-18 15:17:21 +0300

    Main app file updated.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Layout } from '../src/components';
+
+import '../styles/globals.css';
+
+function VladIlie({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default VladIlie;
