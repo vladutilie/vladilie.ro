676934ff8e2de9c636d35860426f05e45bbb1f52
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 16:14:39 +0300

    Navbar component updated.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export const Navbar: React.FC = () => {
   return (
-    <div className='sticky top-0 z-10 mb-8 bg-zinc-100'>
-      <nav className='container mx-auto flex max-w-3xl items-center justify-between px-4 py-2'>
+    <div className='sticky top-0 z-10 mb-16 bg-zinc-100'>
+      <nav className='container mx-auto flex max-w-3xl items-center justify-between px-4 py-3'>
         <Link href=''>
           <a className='inline'>
             <h3>{process.env.NEXT_PUBLIC_SITE_NAME?.split(' ')[0]}</h3>
