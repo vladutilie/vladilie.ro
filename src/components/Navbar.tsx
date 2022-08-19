1025313662a5855c547e8f5e20a96fb0dfe7cfe5
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 09:57:58 +0300

    Navbar z index added.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Navbar: React.FC = () => {
   return (
-    <div className='sticky top-0 bg-zinc-100'>
+    <div className='sticky top-0 z-10 mb-8 bg-zinc-100'>
       <nav className='container mx-auto flex max-w-3xl items-center justify-between px-4 py-2'>
         <Link href=''>
           <a className='inline'>
