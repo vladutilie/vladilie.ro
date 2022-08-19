a9e3034173e6d5e238b9724325e1ca61699bfc18
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 21:57:18 +0300

    New type added.

---
+++ src/types/Page.type.ts
@@ -0,0 +1,12 @@
+type PageType = 'HOME' | 'ABOUT' | 'BLOG' | 'CASE_STUDIES' | 'TOOLS' | 'BOOKS' | 'BOARDGAMES' | 'QUOTES';
+type SocialType = 'GITHUB' | 'INSTAGRAM' | 'LINKEDIN' | 'TWITTER' | 'WORDPRESS';
+
+export type Page = { label: string; href: string };
+
+export type Pages = {
+  [k in PageType]?: Page;
+};
+
+export type Socials = {
+  [k in SocialType]?: Page;
+};
