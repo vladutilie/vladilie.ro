16072e5c718b8479cb04fe3378b2950ddef725c4
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 16:15:18 +0300

    Social media icons added as components.

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+export { BookTruck } from '../../public/logos/projects/BookTruck';
+export { Github } from './Github';
+export { Instagram } from './Instagram';
+export { LinkedIn } from './LinkedIn';
+export { Twitter } from './Twitter';
+export { WordPress } from './WordPress';
