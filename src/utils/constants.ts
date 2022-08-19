339ef7b914f507f456324277783ce25169e81141
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 22:03:09 +0300

    Constants file created.

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+import { Pages, Socials } from '../types/Page.type';
+
+export const PAGES: Pages = {
+  HOME: { label: 'Home', href: '/' },
+  ABOUT: { label: 'About', href: '/about' },
+  BLOG: { label: 'Blog', href: '/blog' },
+  CASE_STUDIES: { label: 'Case studies', href: '/case-studies' },
+  TOOLS: { label: 'Tools', href: '/tools' },
+  BOOKS: { label: 'Books', href: '/books' },
+  BOARDGAMES: { label: 'Boardgames', href: '/boardgames' },
+  QUOTES: { label: 'Quotes', href: '/quotes' }
+};
+
+export const SOCIAL_LINKS: Socials = {
+  GITHUB: { label: 'GitHub', href: process.env.NEXT_PUBLIC_GITHUB as string },
+  INSTAGRAM: { label: 'Instagram', href: process.env.NEXT_PUBLIC_INSTAGRAM as string },
+  LINKEDIN: { label: 'LinkedIn', href: process.env.NEXT_PUBLIC_LINKEDIN as string },
+  TWITTER: { label: 'Twitter', href: process.env.NEXT_PUBLIC_TWITTER as string },
+  WORDPRESS: { label: 'WordPress', href: process.env.NEXT_PUBLIC_WORDPRESS as string }
+};
