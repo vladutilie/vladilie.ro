135ff0851aa14636f0e19b1e329f65a3a05933e8
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 19:50:36 +0300

    CSS fixed in featured service component.

--- src/components/FeaturedServices/FeaturedService.tsx
+++ src/components/FeaturedServices/FeaturedService.tsx
@@ -11,10 +11,10 @@ export const FeaturedService: React.FC<FeaturedServiceType> = ({ image, title, u
           className='flex w-full flex-col justify-center gap-y-8 rounded-md bg-white p-6 transition-transform hover:scale-105 md:flex-row md:gap-x-2'
           target='_blank'
         >
-          <div className='flex w-full items-center justify-center p-2 md:w-1/6'>
+          <div className='flex w-full items-center justify-center md:w-1/6 md:p-2'>
             <Image src={image} placeholder='blur' alt={title} />
           </div>
-          <div className='flex w-5/6 flex-col gap-y-3'>
+          <div className='flex w-full flex-col gap-y-3 md:w-5/6'>
             <div className='flex flex-col items-start gap-y-2 md:flex-row md:items-center md:justify-between'>
               <h3>{title}</h3>
               <p className='text-sm text-blue-500'>{url.replace(/^https\:\/\/(.*?)\/?$/, '$1')}↗</p>
