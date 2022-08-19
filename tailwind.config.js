85fd6ed95a83e08563c7947617a5b7b98739c405
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 16:10:22 +0300

    Tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
         blue: {
           500: '#3598db' // curious blue
         }
+      },
+      animation: {
+        slide: 'slide 50s linear infinite',
+        slide2: 'slide2 50s linear infinite',
+        'slide-slow': 'slide 80s linear infinite',
+        'slide-slow2': 'slide2 80s linear infinite'
+      },
+      keyframes: {
+        slide: {
+          from: { transform: 'translateX(0%)' },
+          to: { transform: 'translateX(-100%)' }
+        },
+        slide2: {
+          from: { transform: 'translateX(100%)' },
+          to: { transform: 'translateX(0%)' }
+        }
       }
     },
     fontFamily: {
