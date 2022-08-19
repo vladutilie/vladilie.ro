228fc2abf9e98994029b5077b7e16fbfa421a2ef
Vladut Ilie <vladilie94@gmail.com>
2022-08-19 21:59:17 +0300

    Favicon removed from index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,18 @@ import Head from 'next/head';
 
 import { FeaturedServices, Hero, Navbar, Projects } from '../src/components';
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>{[process.env.NEXT_PUBLIC_SITE_NAME, process.env.NEXT_PUBLIC_SITE_SUBTITLE].join(' - ')}</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>{[process.env.NEXT_PUBLIC_SITE_NAME, process.env.NEXT_PUBLIC_SITE_SUBTITLE].join(' - ')}</title>
+    </Head>
 
-      <main className='container mx-auto flex max-w-3xl flex-col gap-y-10 px-4'>
-        <Hero />
-        <FeaturedServices />
-        <Projects />
-      </main>
-    </>
-  );
-};
+    <main className='container mx-auto flex max-w-3xl flex-col gap-y-10 px-4'>
+      <Hero />
+      <FeaturedServices />
+      <Projects />
+    </main>
+  </>
+);
 
 export default Home;
