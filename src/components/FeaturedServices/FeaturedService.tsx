d7d424a9c5f07777c138e80de20f9669ea735372
Vladut Ilie <vladilie94@gmail.com>
2022-08-22 21:08:45 +0300

    CSS updated to make links underlined on hover.

--- src/components/FeaturedServices/FeaturedService.tsx
+++ src/components/FeaturedServices/FeaturedService.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { FeaturedServiceType } from '../../types/FeaturedService.type';
+import { Arrow } from '../../icons';
 
 export const FeaturedService: React.FC<FeaturedServiceType> = ({ image, title, url, description }) => {
   return (
     <li>
       <Link href={url}>
         <a
-          className='flex w-full flex-col justify-center gap-y-8 rounded-md bg-white p-6 transition-transform hover:scale-105 md:flex-row md:gap-x-2'
+          className='featured flex w-full flex-col justify-center gap-y-8 rounded-md bg-white p-6 transition-transform hover:scale-105 md:flex-row md:gap-x-2'
           target='_blank'
         >
           <div className='flex w-full items-center justify-center md:w-1/6 md:p-2'>
@@ -17,7 +18,10 @@ export const FeaturedService: React.FC<FeaturedServiceType> = ({ image, title, u
           <div className='flex w-full flex-col gap-y-3 md:w-5/6'>
             <div className='flex flex-col items-start gap-y-2 md:flex-row md:items-center md:justify-between'>
               <h3>{title}</h3>
-              <p className='text-sm text-blue-500'>{url.replace(/^https\:\/\/(.*?)\/?$/, '$1')}↗</p>
+              <p className='text-sm text-blue-500'>
+                {url.replace(/^https\:\/\/(.*?)\/?$/, '$1')}
+                <Arrow className='ml-1 inline w-3 fill-blue-500' />
+              </p>
             </div>
 
             <p className='text-sm text-gray-500'>{description}</p>
