c9d5c255861e80faf9523a80b437edbcf1cfa41d
Vladut Ilie <vladilie94@gmail.com>
2022-08-22 20:59:37 +0300

    NextJS config updated.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.gr-assets.com', 'images-na.ssl-images-amazon.com']
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
