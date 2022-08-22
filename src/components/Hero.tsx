252c0c90efadbca1033686b61a7d78c036a6d1b7
Vladut Ilie <vladilie94@gmail.com>
2022-08-22 15:53:53 +0300

    Hero section updated.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import vlad from '../../public/vlad-ilie.jpg';
-import { Github, Instagram, LinkedIn, Twitter, WordPress } from '../icons';
+import vlad from '../../public/assets/images/vlad-ilie.jpg';
+import { Arrow, Github, Instagram, LinkedIn, Twitter, WordPress } from '../icons';
 
 export const Hero: React.FC = () => {
   return (
@@ -10,19 +10,22 @@ export const Hero: React.FC = () => {
       <div className='w-full md:w-2/3'>
         <h1 className='text-center md:text-left'>Hi there, Vlad here!</h1>
         <p className='mt-3 mb-4 leading-7'>
-          I'm a <strong>Software Engineer</strong> based in Romania, I love to juggle with technologies and put my soul
-          in every project to get the best of it. I'm a{' '}
+          I&apos;m a <strong>Software Engineer</strong> based in Romania, I love to juggle with technologies and put my
+          soul in every project to get the best of it. I&apos;m a{' '}
           <strong>
             <Link href={process.env.NEXT_PUBLIC_NEO4J as string}>
-              <a target='_blank'>Neo4j ↗</a>
+              <a target='_blank'>
+                Neo4j <Arrow className='inline w-3 fill-blue-500' />
+              </a>
             </Link>{' '}
             Certified Professional
           </strong>
           {', '}a <strong>NextJS</strong> fan, an enthusiastic of <strong>JavaScript</strong> development technologies
-          and a nostalgic <strong>WordPress</strong> lover. I'm also the maintainer of{' '}
+          and a nostalgic <strong>WordPress</strong> lover. I&apos;m also the maintainer of{' '}
           <Link href='https://cartilepefata.ro'>
             <a target='_blank'>
-              <strong>Cărțile pe Față</strong> ↗
+              <strong>Cărțile pe Față</strong>
+              <Arrow className='ml-1 inline w-3 fill-blue-500' />
             </a>
           </Link>{' '}
           website network.
