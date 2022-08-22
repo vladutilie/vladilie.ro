a90d98ff007f6a1daec4379648ac4b3742fd7357
Vladut Ilie <vladilie94@gmail.com>
2022-08-22 15:53:21 +0300

    Navbar home link updated.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar: React.FC = () => {
   return (
     <div className='sticky top-0 z-10 mb-16 bg-zinc-100'>
       <nav className='container mx-auto flex max-w-3xl items-center justify-between px-4 py-3'>
-        <Link href=''>
+        <Link href='/'>
           <a className='inline'>
             <h3>{process.env.NEXT_PUBLIC_SITE_NAME?.split(' ')[0]}</h3>
           </a>
