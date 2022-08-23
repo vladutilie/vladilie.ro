c103a3df17166694f7fda0ad667986eab700b6fb
Vladut Ilie <vladilie94@gmail.com>
2022-08-23 14:09:35 +0300

    Tool component CSS updated.

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -10,7 +10,7 @@ export const Tool: React.FC<ToolType> = ({ logo, name, description, url }) => {
       <a className='dev-tool flex w-full flex-col items-center gap-x-6 md:flex-row' target='_blank'>
         <Image src={logo} width={72} height={72} alt={name} className='h-16 w-16' />
 
-        <div>
+        <div className='w-full'>
           <h3>{name}</h3>
           <span>{description}</span>
         </div>
