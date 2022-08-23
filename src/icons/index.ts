14b109a4da6a11734da70e9b0190c28a923d91d4
Vladut Ilie <vladilie94@gmail.com>
2022-08-23 13:49:29 +0300

    New icons added.

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,12 @@
 export { Arrow } from './Arrow';
 export { Done } from './Done';
+export { Duration } from './Duration';
 export { Favorite } from './Favorite';
 export { Github } from './Github';
 export { Glasses } from './Glasses';
 export { Instagram } from './Instagram';
 export { LinkedIn } from './LinkedIn';
+export { Players } from './Players';
 export { Twitter } from './Twitter';
 export { Wishlist } from './Wishlist';
 export { WordPress } from './WordPress';
