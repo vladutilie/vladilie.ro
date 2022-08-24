87181f9c8376526c9c2d667441347df2890a6fdd
Vladut Ilie <vladilie94@gmail.com>
2022-08-24 15:43:44 +0300

    New type added.

---
+++ src/types/Technology.type.ts
@@ -0,0 +1,7 @@
+export type Technology = {
+  blurDataUrl: string;
+  height: number;
+  image: string;
+  name: string;
+  width: number;
+};
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type { Boardgame } from './Boardgame.type';
+export type { Book } from './Book.type';
+export type { FeaturedService } from './FeaturedService.type';
+export type { Page } from './Page.type';
+export type { Project } from './Project.type';
+export type { Technology } from './Technology.type';
+export type { Tool } from './Tool.type';
