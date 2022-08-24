f8582e542a2a68c0c20152701a3f84c8b365bc5f
Vladut Ilie <vladilie94@gmail.com>
2022-08-24 16:11:09 +0300

    Footer links updated.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,13 @@ import Image from 'next/image';
 import { Page } from '../types';
 import { PAGES, SOCIAL_LINKS } from '../utils/constants';
 
-const firstCol: Page[] = [PAGES.HOME as Page, PAGES.ABOUT as Page, PAGES.BLOG as Page, PAGES.CASE_STUDIES as Page];
+const firstCol: Page[] = [
+  PAGES.HOME as Page,
+  PAGES.ABOUT as Page,
+  PAGES.BLOG as Page,
+  PAGES.CASE_STUDIES as Page,
+  PAGES.COLOPHON as Page
+];
 const secondCol: Page[] = [PAGES.TOOLS as Page, PAGES.BOOKS as Page, PAGES.BOARDGAMES as Page];
 const thirdCol: Page[] = [
   SOCIAL_LINKS.GITHUB as Page,
