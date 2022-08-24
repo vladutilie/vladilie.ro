363d9c47fe8745fee612071209cf63b0e43cf050
Vladut Ilie <vladilie94@gmail.com>
2022-08-24 15:44:44 +0300

    SCV components moved to SVG icons.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,29 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import {
-  CSS,
-  GitHub,
-  HTML,
-  JQuery,
-  MongoDB,
-  MySQL,
-  Neo4j,
-  NestJS,
-  NextJS,
-  NodeJS,
-  PHP,
-  PostgreSQL,
-  Prisma,
-  ReactJS,
-  SQLite,
-  Supabase,
-  TailwindCSS,
-  TypeScript
-} from '../technologies';
-import { Project } from '../types/Project.type';
+import { Project, Technology } from '../types';
 
-export const Projects: React.FC<{ projects: Project[] }> = ({ projects }) => {
+type Props = { projects: Project[]; technologies: Technology[] };
+export const Projects: React.FC<Props> = ({ projects, technologies }) => {
   return (
     <section className='flex flex-col gap-y-3'>
       <h3>Projects I&apos;ve built &amp; used technologies</h3>
@@ -68,117 +49,40 @@ export const Projects: React.FC<{ projects: Project[] }> = ({ projects }) => {
 
       <div className='projects relative flex h-10 items-center overflow-x-hidden'>
         <div className='project-stack absolute flex shrink-0 animate-slide-slow justify-around'>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <CSS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <GitHub className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <HTML className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <JQuery className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <MongoDB className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <MySQL className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Neo4j className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NestJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NextJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NodeJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <PHP className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <PostgreSQL className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Prisma className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <ReactJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <SQLite className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Supabase className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <TailwindCSS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <TypeScript className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
+          {technologies.map(({ blurDataUrl, height, image, name, width }: Technology) => (
+            <div key={name} className='flex-inline min-w-max self-center px-3'>
+              <div className='relative h-10'>
+                <Image
+                  alt={`${name} logo`}
+                  className='opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0'
+                  height={height}
+                  placeholder='blur'
+                  blurDataURL={`data:image/svg+xml,${blurDataUrl}`}
+                  src={image}
+                  width={width}
+                  title={name}
+                />
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className='project-stack absolute flex shrink-0 translate-x-1/2 animate-slide-slow2 justify-around'>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <CSS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <GitHub className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <HTML className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <JQuery className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <MongoDB className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <MySQL className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Neo4j className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NestJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NextJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <NodeJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <PHP className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <PostgreSQL className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Prisma className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <ReactJS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <SQLite className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <Supabase className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <TailwindCSS className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
-          <div className='flex-inline min-w-max self-center px-3'>
-            <TypeScript className='flex-inline h-10 opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0' />
-          </div>
+          {technologies.map(({ blurDataUrl, height, image, name, width }: Technology) => (
+            <div key={name} className='flex-inline min-w-max self-center px-3'>
+              <div className='relative h-10'>
+                <Image
+                  alt={`${name} logo`}
+                  className='opacity-50 grayscale transition-opacity hover:opacity-100 hover:grayscale-0'
+                  height={height}
+                  placeholder='blur'
+                  blurDataURL={`data:image/svg+xml,${blurDataUrl}`}
+                  src={image}
+                  width={width}
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
