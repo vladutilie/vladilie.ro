68c9d9faffd2fe90f3ada1f5b9a7adcb03d929fa
Vladut Ilie <vladilie94@gmail.com>
2022-08-25 12:11:18 +0300

    MDX components updated.

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -2,11 +2,9 @@ import NextImage from 'next/image';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneLight } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
-const Pre = (props: React.PropsWithChildren) => <pre className='overflow-x-auto rounded-md bg-white' {...props} />;
-
 const Code = ({ children: codeString, className: language }: any) => {
   if (!language) {
-    return <code>{codeString}</code>;
+    return <code className='rounded-md bg-white px-1 py-0.5 text-blue-300'>{codeString}</code>;
   }
   language = language.replace('language-', '');
   const showLineNumbers = !['shell', 'text'].includes(language);
@@ -25,7 +23,6 @@ const Image = (props: any) => {
 export const MDXComponents = {
   h2: (props: any) => <h2 id={props.children.toLowerCase().replaceAll(' ', '-')}>{props.children}</h2>,
   code: Code,
-  pre: Pre,
   img: Image,
   ul: (props: any) => <ul className='ml-4 list-disc space-y-2' {...props} />,
   blockquote: (props: any) => (
