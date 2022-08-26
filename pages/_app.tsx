5162dbd53ccf16917fd80361a2358c764120a9c0
Vladut Ilie <vladilie94@gmail.com>
2022-08-26 16:05:21 +0300

    SEO added in main app file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 import { Layout } from '../src/components';
+import { SEO_DESCRIPTION } from '../src/utils/constants';
 
 import '../styles/globals.css';
 
@@ -11,6 +13,31 @@ function VladIlie({ Component, pageProps }: AppProps) {
       <Head>
         <link rel='icon' href='/assets/images/favicon.ico' />
       </Head>
+
+      <NextSeo
+        title={[process.env.NEXT_PUBLIC_SITE_NAME, process.env.NEXT_PUBLIC_SITE_SUBTITLE].join(' - ')}
+        description={SEO_DESCRIPTION}
+        twitter={{
+          cardType: 'summary_large_image',
+          handle: '@vladilie94'
+        }}
+        openGraph={{
+          url: process.env.NEXT_PUBLIC_SITE_URL,
+          title: [process.env.NEXT_PUBLIC_SITE_NAME, process.env.NEXT_PUBLIC_SITE_SUBTITLE].join(' - '),
+          description: SEO_DESCRIPTION,
+          locale: 'en_US',
+          images: [
+            {
+              url: 'https://vladilie.ro/assets/images/social-card.png',
+              width: 1200,
+              height: 630,
+              alt: process.env.NEXT_PUBLIC_SITE_NAME,
+              type: 'image/png'
+            }
+          ]
+        }}
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
