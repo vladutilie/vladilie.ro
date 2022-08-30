fb241a9d834d276b0a0a915bba8523f30114a443
Vladut Ilie <vladilie94@gmail.com>
2022-08-30 08:03:47 +0300

    Log added in settings API.

--- pages/api/settings.ts
+++ pages/api/settings.ts
@@ -45,6 +45,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         }
       }
 
+      console.info('Google API', result);
+
       const currentLocation = `${result.locality}, ${result.administrative_area_level_1}`;
       await prisma.settings.upsert({
         where: { name: 'location' },
