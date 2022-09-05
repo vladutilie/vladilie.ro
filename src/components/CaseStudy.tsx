f6e548026fbabdb5e548dca768edf03bf1ff69f3
Vladut Ilie <vladilie94@gmail.com>
2022-09-05 15:52:23 +0300

    Views added in case study list.

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import { format } from 'timeago.js';
 
+import { LoadingDots } from './';
 import { CaseStudy as CaseStudyType } from '../types';
+import { usePostViews } from '../hooks/usePostViews';
 
 export const CaseStudy: React.FC<CaseStudyType> = ({ title, description, date, slug, readingTime }) => {
+  const { views, isLoading, isError } = usePostViews(slug);
+
   return (
     <Link href={`/case-studies/${slug}`}>
       <a className='flex flex-col gap-y-2 rounded-md p-4 transition-all hover:scale-105 hover:bg-white'>
         <h3>{title}</h3>
-        <span>
-          {format(date)} &bull; {readingTime}
-        </span>
+
+        <div>
+          <span>{format(date)} &bull; </span>
+          <span>{isError || isLoading ? <LoadingDots /> : views} views </span>
+          <span>&bull; {readingTime}</span>
+        </div>
+
         <span>{description}</span>
       </a>
     </Link>
