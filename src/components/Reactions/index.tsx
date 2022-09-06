0709b006e652bddae60793dfda6e0b7e72300cb6
Vladut Ilie <vladilie94@gmail.com>
2022-09-06 22:53:14 +0300

    Reactions component created.

---
+++ src/components/Reactions/index.tsx
@@ -0,0 +1,29 @@
+import { usePostLikes } from '../../hooks/usePostReactions';
+import { Reaction as ReactionType } from '../../types';
+import { ReactionEmoji } from '../../types/Reaction.type';
+import { Reaction } from './Reaction';
+
+const reactions: { reaction: ReactionType; emoji: ReactionEmoji }[] = [
+  { reaction: 'likes', emoji: '👍' },
+  { reaction: 'loves', emoji: '❤️' },
+  { reaction: 'awards', emoji: '🏆' },
+  { reaction: 'wows', emoji: '😲' }
+];
+export const Reactions: React.FC<{ slug: string }> = ({ slug }) => {
+  const { counter, session, isLoading, react } = usePostLikes(slug);
+
+  return (
+    <div className='flex gap-x-3'>
+      {reactions.map(({ reaction, emoji }) => (
+        <Reaction
+          key={reaction}
+          reaction={reaction}
+          emoji={emoji}
+          session={session?.[reaction]}
+          counter={counter?.[reaction]}
+          react={react}
+        />
+      ))}
+    </div>
+  );
+};
