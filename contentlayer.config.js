d0ed0ef7f0e1cd79910e5c95e94fd37a4d21b6aa
Vladut Ilie <vladilie94@gmail.com>
2022-09-08 13:58:44 +0300

    Content layer config updated.

---
+++ contentlayer.config.js
@@ -0,0 +1,43 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import rehypePrettyCode from 'rehype-pretty-code';
+import readingTime from 'reading-time';
+
+import { rehypePrettyCodeOptions } from './src/utils/rehypePrettyCode';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  contentType: 'mdx',
+  filePathPattern: 'blog/*.mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    description: { type: 'string', required: true }
+  },
+  computedFields: {
+    slug: { type: 'string', resolve: (post) => post._raw.sourceFileName.replace(/\.mdx$/, '') },
+    readingTime: { type: 'string', resolve: (post) => readingTime(post.body.raw).text }
+  }
+}));
+
+export const CaseStudy = defineDocumentType(() => ({
+  name: 'CaseStudy',
+  contentType: 'mdx',
+  filePathPattern: 'case-studies/*.mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    description: { type: 'string', required: true }
+  },
+  computedFields: {
+    slug: { type: 'string', resolve: (post) => post._raw.sourceFileName.replace(/\.mdx$/, '') },
+    readingTime: { type: 'string', resolve: (post) => readingTime(post.body.raw).text }
+  }
+}));
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Post, CaseStudy],
+  mdx: {
+    rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]]
+  }
+});
