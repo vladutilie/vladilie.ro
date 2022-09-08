112ac9c9a935bd887932cb0791d40bd9cd3f82fa
Vladut Ilie <vladilie94@gmail.com>
2022-09-08 14:00:27 +0300

    Useless function files removed.

--- src/utils/readCaseStudy.ts
+++
@@ -1,8 +0,0 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-
-export const readCaseStudy = async (slug: string) => {
-  const postPath = path.join(process.cwd(), './public/data/case-studies', slug, 'index.mdx');
-
-  return await fs.readFile(postPath, 'utf8');
-};
