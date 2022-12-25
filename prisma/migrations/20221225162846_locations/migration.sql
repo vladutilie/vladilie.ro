30fb95ab6f3078a1865e2cf59cc737857d19028b
Vladut Ilie <vladilie94@gmail.com>
2022-12-26 00:01:12 +0200

    Migration fixed.

--- prisma/migrations/20221225162846_locations/migration.sql
+++ prisma/migrations/20221225162846_locations/migration.sql
@@ -1,6 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the `Settings` table. If the table is not empty, all the data it contains will be lost.
+
+*/
 -- AlterTable
-ALTER TABLE `sessions` RENAME COLUMN `createdAt` to `created_at`,
-    RENAME COLUMN `updatedAt` to `updated_at`;
+ALTER TABLE `sessions` DROP COLUMN `createdAt`,
+    DROP COLUMN `updatedAt`,
+    ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
 
 -- DropTable
 DROP TABLE `Settings`;
