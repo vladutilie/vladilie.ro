48914861df5b0368e9592c9e9a3380ad01c24aa2
Vladut Ilie <vladilie94@gmail.com>
2023-11-17 14:26:51 +0200

    feat: icons implemented

---
+++ src/app/[locale]/ui/icons/index.ts
@@ -0,0 +1,8 @@
+export { Burger } from './burger.icon';
+export { GitHub } from './github.icon';
+export { LinkedIn } from './linkedin.icon';
+export { Logo } from './logo.icon';
+export { Pin } from './pin.icon';
+export { Polywork } from './polywork.icon';
+export { WordPress } from './wordpress.icon';
+export { X } from './x.icon';
