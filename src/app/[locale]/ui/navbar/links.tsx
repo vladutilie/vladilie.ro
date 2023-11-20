e9b4eb1702bd751f9f1569f8c007a4149cba57bd
Vladut Ilie <vladilie94@gmail.com>
2023-11-20 16:20:07 +0200

    refactor: navbar links improved

--- src/app/[locale]/ui/navbar/links.tsx
+++ src/app/[locale]/ui/navbar/links.tsx
@@ -1,49 +1,28 @@
 import { useTranslations } from 'next-intl';
-import { Link } from '@/navigation';
 
 import { ThemeSwitcher } from './theme-switcher';
 import { LangChanger } from './lang-changer';
+import { scrollTo } from '@/lib/utils';
+import { NavLinks } from '@/lib/constants';
 
 export const Links: React.FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
   const t = useTranslations('navbar');
 
-  // TODO: Replace Link with simple <a> where there is no navigation between pages.
   return (
     <ul
       className={`flex-col gap-x-6 after:absolute after:left-0 after:w-full after:border-b after:dark:border-gray-600 md:flex-row md:after:hidden ${
         isMobile ? 'flex gap-y-4 overflow-y-auto text-base' : 'hidden items-center md:flex'
       }`}
     >
-      <li className='pt-4 md:pt-0'>
-        <Link href='/' className='text-inherit'>
-          {t('about')}
-        </Link>
-      </li>
-      <li>
-        <Link href='/' className='text-inherit'>
-          {t('education')}
-        </Link>
-      </li>
-      <li>
-        <Link href='/' className='text-inherit'>
-          {t('skills')}
-        </Link>
-      </li>
-      <li>
-        <Link href='/blog' className='text-inherit'>
-          {t('projects')}
-        </Link>
-      </li>
-      <li>
-        <Link href='/' className='text-inherit'>
-          {t('services')}
-        </Link>
-      </li>
-      <li>
-        <Link href='/' className='text-inherit'>
-          {t('contact')}
-        </Link>
-      </li>
+      {Object.values(NavLinks).map((item, idx) => (
+        <li
+          className={`cursor-pointer transition-colors hover:text-blue-500 ${!idx ? 'pt-4 md:pt-0' : ''}`}
+          key={idx}
+          onClick={() => scrollTo(item)}
+        >
+          {t(item)}
+        </li>
+      ))}
       <li className='hidden h-6 w-0.5 bg-gray-100 md:block' />
       <li className='before:absolute before:left-0 before:w-full before:border-b before:dark:border-gray-600 md:before:hidden'>
         <ThemeSwitcher />
