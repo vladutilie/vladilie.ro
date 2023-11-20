1ffa21aee6de66f331e4f48ff6f4e9a2cf214da3
Vladut Ilie <vladilie94@gmail.com>
2023-11-20 16:18:53 +0200

    chore: navlinks added in constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const NavLinks = {
+  About: 'about',
+  Education: 'education',
+  Experience: 'experience',
+  Projects: 'projects',
+  Services: 'services',
+  Blog: 'blog',
+  Contact: 'contact'
+} as const;
