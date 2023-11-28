0dac32b4bee1fb35367cf3bad836ed779e6ac9cd
Vladut Ilie <vladilie94@gmail.com>
2023-11-28 22:52:38 +0200

    feat: search for blog implemented

---
+++ src/app/[locale]/blog/ui/Search.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useTranslations } from 'next-intl';
+
+export const Search: React.FC = () => {
+  const t = useTranslations('blog');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    console.log(window.location.search);
+  }, []);
+
+  const handleInput = (value: string): void => {
+    setQuery(value);
+  };
+
+  return (
+    <input
+      type='text'
+      autoFocus
+      placeholder={t('search-placeholder')}
+      className='h-10 rounded-md bg-gray-50 px-2 py-1 outline-none dark:bg-slate-900'
+      value={query}
+      onChange={({ currentTarget }) => handleInput(currentTarget.value)}
+    />
+  );
+};
