35ca4e4d57f5b0c8f4f3e72b38fe416bb1d339ed
Vladut Ilie <vladilie94@gmail.com>
2023-11-28 17:16:33 +0200

    feat: pages added in config

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,16 @@ export const NavLinks = {
   Education: 'education',
   Skills: 'skills',
   Projects: 'projects',
-  Services: 'services',
   Blog: 'blog',
   Contact: 'contact'
 } as const;
+
+export const Pages = [
+  { i18nKey: 'blog', url: '/blog' },
+  { i18nKey: 'projects', url: '/projects' },
+  { i18nKey: 'contact', url: '/contact' },
+  { i18nKey: 'colophon', url: '/colophon' },
+  { i18nKey: 'uses', url: '/uses' },
+  { i18nKey: 'books', url: '/books' },
+  { i18nKey: 'boardgames', url: '/boardgames' }
+] as const;
--- src/navigation.ts
+++ src/navigation.ts
@@ -5,8 +5,15 @@ export const locales = ['en', 'ro'] as const;
 export const pathnames = {
   '/': '/',
   '/location': { en: '/locations', ro: '/locatii' },
+  '/blog': '/blog',
   '/projects': { en: '/projects', ro: '/proiecte' },
-  '/blog': '/blog'
+  '/contact': '/contact',
+  '/colophon': { en: '/colophon', ro: '/colofon' },
+  '/uses': { en: '/uses', ro: '/utilizari' },
+  '/books': { en: '/books', ro: '/carti' },
+  '/boardgames': { en: '/boardgames', ro: '/jocuri-societate' },
+  '/terms': { en: '/terms', ro: '/termeni' },
+  '/privacy': { en: '/privacy', ro: '/confidentialitate' }
 } satisfies Pathnames<typeof locales>;
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createLocalizedPathnamesNavigation({
