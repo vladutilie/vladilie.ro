bdc9f901a48ddaeba33456373a6d4039bde529ef
Vladut Ilie <vladilie94@gmail.com>
2023-11-29 17:55:51 +0200

    feat: loading dots component implemented

--- src/app/[locale]/ui/loading-dots.tsx
+++ src/app/[locale]/ui/loading-dots.tsx
@@ -1,7 +1,7 @@
 export const LoadingDots: React.FC = () => (
-  <>
-    <span className='inline-flex animate-[loading_1.4s_ease-in-out_infinite] rounded-full'>&bull;</span>
-    <span className='inline-flex animate-[loading_1.4s_ease-in-out_0.2s_infinite] rounded-full'>&bull;</span>
-    <span className='inline-flex animate-[loading_1.4s_ease-in-out_0.4s_infinite] rounded-full'>&bull;</span>
-  </>
+  <div className='inline text-xs text-gray-400'>
+    <span className='animate-[loading_1.4s_ease-in-out_infinite] rounded-full '>&bull;</span>
+    <span className='animate-[loading_1.4s_ease-in-out_0.2s_infinite] rounded-full'>&bull;</span>
+    <span className='animate-[loading_1.4s_ease-in-out_0.4s_infinite] rounded-full'>&bull;</span>
+  </div>
 );
