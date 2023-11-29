bdc4b5c24e403c54dbfbc9451ab377b09b939784
Vladut Ilie <vladilie94@gmail.com>
2023-11-29 22:48:53 +0200

    feat: blog page updated

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -8,18 +8,18 @@ export default function Blog() {
   const t = useTranslations('blog');
 
   return (
-    <section className='container mx-auto flex max-w-7xl flex-col gap-8 px-4 py-16 md:py-32'>
+    <section className='container mx-auto flex max-w-7xl flex-col gap-8 px-4 py-20 md:py-32'>
       <div className='flex items-center justify-between gap-x-4'>
         <h1>Blog</h1>
         {/* <Search /> */}
       </div>
 
       {allPosts.length ? (
-        <div className='flex flex-col gap-y-4'>
+        <ul className='flex flex-col gap-y-4'>
           {allPosts.map((post: PostType, idx: number) => (
             <PostCard key={idx} {...post} />
           ))}
-        </div>
+        </ul>
       ) : (
         <p>{t('no-posts')}</p>
       )}
