8a9146f37320112cd12d71090ac788acf148f220
Vladut Ilie <vladilie94@gmail.com>
2023-11-29 19:33:41 +0200

    feat: post component improved

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { useMDXComponent } from 'next-contentlayer/hooks';
 import { PostMeta } from '../../ui/post-meta';
 import { Reactions } from '../../ui/reactions';
 
-export default function Post({ params: { slug, locale } }: { params: { slug: string; locale: string } }) {
+export default function Post({ params: { slug } }: { params: { slug: string } }) {
   const post = allPosts.find((post: Post) => post.slug === slug);
 
   if (!post) {
@@ -16,28 +16,29 @@ export default function Post({ params: { slug, locale } }: { params: { slug: str
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <section className='container mx-auto max-w-4xl px-4 py-16 md:py-32'>
+    <article className='container mx-auto max-w-4xl px-4 py-16 md:py-32'>
       <div className='flex flex-col gap-y-4'>
         <div className='relative h-96 w-full'>
           <Image
             alt={post.title}
             blurDataURL={post.blurDataImage}
-            className='rounded-md border object-cover'
+            className='rounded-md object-cover shadow-lg'
             fill
             placeholder='blur'
+            sizes='100vw'
             src={post.featuredImage}
           />
         </div>
 
-        <h1 className='text-4xl'>{post.title}</h1>
+        <h1>{post.title}</h1>
 
-        <div className='flex flex-col items-center justify-between md:flex-row'>
+        <div className='mb-4 flex flex-col justify-between gap-y-2 border-b pb-6 sm:flex-row sm:items-center'>
           <Reactions slug={slug} />
           <PostMeta date={post.date} readingTime={post.readingTime} slug={slug} />
         </div>
       </div>
 
       <MDXContent />
-    </section>
+    </article>
   );
 }
