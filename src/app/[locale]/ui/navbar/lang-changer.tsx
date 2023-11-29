79eaef1d6984a054d7c1dfb9ad17f8bb9e926338
Vladut Ilie <vladilie94@gmail.com>
2023-11-29 17:54:18 +0200

    fix: language changes component fixed

--- src/app/[locale]/ui/navbar/lang-changer.tsx
+++ src/app/[locale]/ui/navbar/lang-changer.tsx
@@ -1,18 +1,20 @@
 import { useLocale, useTranslations } from 'next-intl';
-import { Link, locales } from '@/navigation';
-import { usePathname } from '@/navigation';
+import { useParams } from 'next/navigation';
+import { Link, locales, usePathname } from '@/navigation';
 
 export const LangChanger: React.FC = () => {
   const t = useTranslations('navbar');
   const locale = useLocale();
   const pathname = usePathname();
+  const params = useParams();
 
   return (
     <div className='flex items-center justify-between'>
       <span className='transition-colors md:hidden'>{t('change-language')}</span>
 
       <Link
-        href={pathname}
+        // @ts-ignore
+        href={{ pathname, params }}
         locale={locales[0] === locale ? 'ro' : 'en'}
         className='px-2 text-2xl md:px-0'
         title={t('change-language-to', { lang: locales[0] === locale ? 'română' : 'English' })}
