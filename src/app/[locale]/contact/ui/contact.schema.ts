b2f95da747b1ffb1e002858b6b0422f4e5534323
Vladut Ilie <vladilie94@gmail.com>
2023-12-04 19:01:36 +0200

    feat: contact schema implemented

---
+++ src/app/[locale]/contact/ui/contact.schema.ts
@@ -0,0 +1,24 @@
+import { number, object, string } from 'yup';
+
+import { Inputs } from './inputs.type';
+import { ContactServices } from '@/utils/constants';
+
+export const ContactSchema = object<Inputs>({
+  firstName: string().trim().min(4).max(20).required(),
+  lastName: string().trim().min(4).max(20).required(),
+  email: string().email().required(),
+  phone: string().min(10).max(15).required(),
+  service: string()
+    .oneOf([
+      ContactServices.CustomDevelopment,
+      ContactServices.LandingPage,
+      ContactServices.Maintenance,
+      ContactServices.JobOpportunity
+    ])
+    .required(),
+  budget: number().when('service', {
+    is: (value: string) => value !== ContactServices.JobOpportunity,
+    then: (schema) => schema.min(1000).max(30000)
+  }),
+  message: string().trim().required()
+});
