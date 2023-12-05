9e749b3cfd173e25f36c17ef3b3499725a10c046
Vladut Ilie <vladilie94@gmail.com>
2023-12-05 16:20:58 +0200

    feat: disable lang switcher when on blog or project post

--- src/app/[locale]/ui/navbar/lang-changer.tsx
+++ src/app/[locale]/ui/navbar/lang-changer.tsx
@@ -2,6 +2,8 @@ import { useLocale, useTranslations } from 'next-intl';
 import { useParams } from 'next/navigation';
 import { Link, locales, usePathname } from '@/navigation';
 
+const notAllowedPaths = ['/blog/[slug]', '/projects/[slug]'];
+
 export const LangChanger: React.FC = () => {
   const t = useTranslations('navbar');
   const locale = useLocale();
@@ -16,7 +18,7 @@ export const LangChanger: React.FC = () => {
         // @ts-ignore
         href={{ pathname, params }}
         locale={locales[0] === locale ? 'ro' : 'en'}
-        className='px-2 text-2xl md:px-0'
+        className={`px-2 text-2xl md:px-0 ${notAllowedPaths.includes(pathname) ? 'pointer-events-none grayscale' : ''}`}
         title={t('change-language-to', { lang: locales[0] === locale ? 'română' : 'English' })}
       >
         {locales[0] === locale ? '🇷🇴' : '🇺🇸'}
