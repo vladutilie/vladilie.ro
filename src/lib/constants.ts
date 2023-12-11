88aca2163f4b7df91c8e52ad76028483f9e6b611
Vladut Ilie <vladilie94@gmail.com>
2023-12-11 10:52:31 +0200

    refactor: contact services removed from constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,10 +14,3 @@ export const Pages = {
   Books: 'books',
   Boardgames: 'boardgames'
 } as const;
-
-export const ContactServices = {
-  CustomDevelopment: 'custom-development',
-  LandingPage: 'landing-page',
-  Maintenance: 'maintenance',
-  JobOpportunity: 'job-opportunity'
-} as const;
