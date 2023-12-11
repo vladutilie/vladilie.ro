f8c85a2a3dbccc07d127818035f45ead198b6d28
Vladut Ilie <vladilie94@gmail.com>
2023-12-11 10:52:16 +0200

    fix: sw localization link to contact page

--- src/app/[locale]/ui/sw-l10n/quote.tsx
+++ src/app/[locale]/ui/sw-l10n/quote.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { useTranslations } from 'next-intl';
+import { Link } from '@/navigation';
 
 export const Quote: React.FC = () => {
   const t = useTranslations('homepage.sw-l10n');
@@ -8,13 +9,12 @@ export const Quote: React.FC = () => {
     <p className='text-center'>
       {t.rich('quote', {
         contact: (chunk: ReactNode): JSX.Element => (
-          <a
-            href='#'
-            target='_blank'
+          <Link
+            href='/contact'
             className='animate-text-bg text-clip bg-gradient-to-r from-blue-450 via-gray-600 to-green-500 bg-[length:400%] bg-clip-text text-lg font-semibold text-transparent hover:text-transparent'
           >
             {chunk}
-          </a>
+          </Link>
         )
       })}
     </p>
