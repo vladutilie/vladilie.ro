df0d0cd064f0c272bb5860d586d5daad1c17d502
Vladut Ilie <vladilie94@gmail.com>
2023-12-11 10:51:29 +0200

    refactor: contact services moved in an enum

--- src/app/[locale]/contact/ui/contact.schema.ts
+++ src/app/[locale]/contact/ui/contact.schema.ts
@@ -1,7 +1,7 @@
 import { number, object, string } from 'yup';
 
 import { Inputs } from './inputs.type';
-import { ContactServices } from '@/lib/constants';
+import { Services } from './services.enum';
 
 export const ContactSchema = object<Inputs>({
   firstName: string().trim().min(4).max(20).required(),
@@ -10,14 +10,16 @@ export const ContactSchema = object<Inputs>({
   phone: string().min(10).max(15).required(),
   service: string()
     .oneOf([
-      ContactServices.CustomDevelopment,
-      ContactServices.LandingPage,
-      ContactServices.Maintenance,
-      ContactServices.JobOpportunity
+      Services.CustomDevelopment,
+      Services.LandingPage,
+      Services.SwI18nL10n,
+      Services.Maintenance,
+      Services.JobOpportunity,
+      Services.JustSayHi
     ])
     .required(),
   budget: number().when('service', {
-    is: (value: string) => value !== ContactServices.JobOpportunity,
+    is: (value: string) => ![Services.JobOpportunity, Services.JustSayHi].includes(value as Services),
     then: (schema) => schema.min(1000).max(30000)
   }),
   message: string().trim().required()
--- src/app/[locale]/contact/ui/inputs.type.ts
+++ src/app/[locale]/contact/ui/inputs.type.ts
@@ -1,9 +1,11 @@
+import { Services } from './services.enum';
+
 export type Inputs = {
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-  service: string;
+  service?: Services;
   budget?: number;
   message: string;
 };
