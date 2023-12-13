e3791db6ab202623d308ba4645b7e4d6efbccbc7
Vladut Ilie <vladilie94@gmail.com>
2023-12-13 14:10:05 +0200

    fix: id removed from projects section

--- src/app/[locale]/ui/projects/index.tsx
+++ src/app/[locale]/ui/projects/index.tsx
@@ -2,7 +2,6 @@ import { useLocale, useTranslations } from 'next-intl';
 import { Link } from '@/navigation';
 import { allProjects } from 'contentlayer/generated';
 
-import { NavLinks } from '@/lib/constants';
 import { ProjectCard } from './project-card';
 
 export const Projects: React.FC = () => {
@@ -11,10 +10,7 @@ export const Projects: React.FC = () => {
   const projects = allProjects.filter(({ locale: l }) => l === locale).slice(0, 3);
 
   return (
-    <section
-      id={NavLinks.Projects}
-      className='container mx-auto flex max-w-7xl flex-col gap-8 px-4 py-16 md:py-20 2xl:py-24'
-    >
+    <section className='container mx-auto flex max-w-7xl flex-col gap-8 px-4 py-16 md:py-20 2xl:py-24'>
       <div className='col-span-3 text-center'>
         <span className='rounded-xl bg-gray-600 px-4 py-1 text-sm text-gray-200'>{t('tag')}</span>
       </div>
