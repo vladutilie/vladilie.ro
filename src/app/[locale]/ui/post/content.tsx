f4a36d1d162fb72aa619ba37cf0a01454f35f380
Vladut Ilie <vladilie94@gmail.com>
2024-01-29 16:17:43 +0200

    feat: MDX components prop passed

--- src/app/[locale]/ui/post/content.tsx
+++ src/app/[locale]/ui/post/content.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 
 export const Content: React.FC<{ content: string }> = ({ content }) => {
   const MDXContent = useMDXComponent(content);
 
-  return <MDXContent />;
+  const components = {
+    Link,
+    ul: (props: any) => <ul className='list-inside list-disc' {...props} />,
+    ol: (props: any) => <ul className='list-inside list-decimal' {...props} />
+  };
+
+  // @ts-ignore
+  return <MDXContent components={{ ...components }} />;
 };
