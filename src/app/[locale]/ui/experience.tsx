2d30f7c23d992d86ddd223aa3281febb5ac50344
Vladut Ilie <vladilie94@gmail.com>
2025-01-28 12:14:05 +0200

    feat: file paths import updated

--- src/app/[locale]/ui/experience.tsx
+++ src/app/[locale]/ui/experience.tsx
@@ -5,20 +5,20 @@ import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 import { NavLinks } from '@/lib/constants';
 
-import javascript from '@/../../public/images/technologies/javascript.svg';
-import typescript from '@/../../public/images/technologies/typescript.svg';
-import nodejs from '@/../../public/images/technologies/nodejs.svg';
+import javascript from '@/../public/images/technologies/javascript.svg';
+import typescript from '@/../public/images/technologies/typescript.svg';
+import nodejs from '@/../public/images/technologies/nodejs.svg';
 import { ExpressJS, PHP, WordPress2, MySQL, Prisma } from './icons';
-import nextjs from '@/../../public/images/technologies/nextjs.svg';
-import nestjs from '@/../../public/images/technologies/nestjs.svg';
-import neo4j from '@/../../public/images/technologies/neo4j.svg';
-import mongodb from '@/../../public/images/technologies/mongodb.svg';
-import postgresql from '@/../../public/images/technologies/postgresql.svg';
-import sqlite from '@/../../public/images/technologies/sqlite.svg';
-import supabase from '@/../../public/images/technologies/supabase.svg';
-import reactjs from '@/../../public/images/technologies/reactjs.svg';
-import tailwindcss from '@/../../public/images/technologies/tailwindcss.svg';
-import git from '@/../../public/images/technologies/git.svg';
+import nextjs from '@/../public/images/technologies/nextjs.svg';
+import nestjs from '@/../public/images/technologies/nestjs.svg';
+import neo4j from '@/../public/images/technologies/neo4j.svg';
+import mongodb from '@/../public/images/technologies/mongodb.svg';
+import postgresql from '@/../public/images/technologies/postgresql.svg';
+import sqlite from '@/../public/images/technologies/sqlite.svg';
+import supabase from '@/../public/images/technologies/supabase.svg';
+import reactjs from '@/../public/images/technologies/reactjs.svg';
+import tailwindcss from '@/../public/images/technologies/tailwindcss.svg';
+import git from '@/../public/images/technologies/git.svg';
 
 const technologies: { name: string; image: StaticImport; url: string }[] = [
   { name: 'JavaScript', image: javascript, url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
